7e60d35e4bbb2d35c1b0103218f10b143e1654e5
fitronansori <fitronansori7@gmail.com>
2024-06-06 21:55:03 +0800

    feat: Add Subscribe component to homepage

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -3,6 +3,7 @@ import Benefit from "./_components/Benefit";
 import Hero from "./_components/Hero";
 import Join from "./_components/Join";
 import Reading from "./_components/Reading";
+import Subscribe from "./_components/Subscribe";
 import TreeCount from "./_components/TreeCount";
 
 export default function Home() {
@@ -14,6 +15,7 @@ export default function Home() {
       <Join />
       <Benefit />
       <Reading />
+      <Subscribe />
     </>
   );
 }
