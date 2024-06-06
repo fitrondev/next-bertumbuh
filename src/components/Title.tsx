c84e94b2250f6186db9e119750396cee0e0910ff
fitronansori <fitronansori7@gmail.com>
2024-06-06 08:52:28 +0800

    feat: Add Title component

---
+++ src/components/Title.tsx
@@ -0,0 +1,28 @@
+import { cn } from "@/lib/utils";
+
+const Title = ({
+  section,
+  title,
+  description,
+  className,
+}: {
+  section: string;
+  title: string;
+  description?: string;
+  className?: string;
+}) => {
+  return (
+    <div
+      className={cn(
+        "space-y-2 flex flex-col items-center justify-center lg:items-start lg:justify-start",
+        className
+      )}>
+      <span className="text-lg sm:text-2xl text-primary font-medium">
+        {section}
+      </span>
+      <h2 className="text-xl sm:text-4xl lg:text-5xl font-medium">{title}</h2>
+      <p>{description}</p>
+    </div>
+  );
+};
+export default Title;
