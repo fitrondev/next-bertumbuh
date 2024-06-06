08599ea09e6e965664b92be700b95f1173fac3f6
fitronansori <fitronansori7@gmail.com>
2024-06-06 19:25:11 +0800

    refactor: Update Title component to include custom class names

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -5,19 +5,35 @@ const Title = ({
   title,
   description,
   className,
+  titleClassName,
+  descClassName,
 }: {
   section: string;
   title: string;
   description?: string;
   className?: string;
+  titleClassName?: string;
+  descClassName?: string;
 }) => {
   return (
     <div className={cn("space-y-2", className)}>
       <span className="text-lg sm:text-xl lg:text-2xl text-primary font-medium">
         {section}
       </span>
-      <h2 className="text-2xl sm:text-4xl lg:text-5xl font-medium">{title}</h2>
-      <p>{description}</p>
+      <h2
+        className={cn(
+          "text-2xl sm:text-4xl lg:text-5xl font-medium",
+          titleClassName
+        )}>
+        {title}
+      </h2>
+      <p
+        className={cn(
+          "text-sm sm:text-base text-muted-foreground",
+          descClassName
+        )}>
+        {description}
+      </p>
     </div>
   );
 };
