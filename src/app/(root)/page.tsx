88193dd3d3b1781f83d44b6970f93875f4404865
fitronansori <fitronansori7@gmail.com>
2024-06-06 09:34:59 +0800

    feat: Add Join component and update Home page to include it

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,5 +1,6 @@
 import About from "./_components/About";
 import Hero from "./_components/Hero";
+import Join from "./_components/Join";
 import TreeCount from "./_components/TreeCount";
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <TreeCount />
       <About />
+      <Join />
     </>
   );
 }
