eea668292ae640b36e0936a69edf171f57e14980
fitronansori <fitronansori7@gmail.com>
2024-06-06 21:54:34 +0800

    refactor: Update SearchForm component imports

--- src/components/layouts/SearchForm/index.tsx
+++ src/components/layouts/SearchForm/index.tsx
@@ -6,15 +6,7 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 
 import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Search } from "lucide-react";
 
